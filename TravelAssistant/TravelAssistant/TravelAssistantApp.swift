4ebfb3f8492670af32813febe12708f2ad165740
Abby <chunpinlgai0211@gmail.com>
2025-08-03 10:05:55 +0800

    new project files

---
+++ TravelAssistant/TravelAssistant/TravelAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAssistantApp.swift
+//  TravelAssistant
+//
+//  Created by Abby on 2025/8/3.
+//
+
+import SwiftUI
+
+@main
+struct TravelAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TravelAssistant/TravelAssistantUITests/TravelAssistantUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  TravelAssistantUITestsLaunchTests.swift
+//  TravelAssistantUITests
+//
+//  Created by Abby on 2025/8/3.
+//
+
+import XCTest
+
+final class TravelAssistantUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
